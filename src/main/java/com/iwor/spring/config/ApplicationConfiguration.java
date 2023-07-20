7a89787dacc2867d33cb21462c57df3bf2d084ea
Iw <iwor.java@gmail.com>
2023-07-20 10:41:11 +0400

    add Configuration Imports

--- src/main/java/com/iwor/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/iwor/spring/config/ApplicationConfiguration.java
@@ -1,15 +1,20 @@
 package com.iwor.spring.config;
 
 import com.iwor.spring.database.repository.CrudRepository;
+import com.iwor.web.config.WebConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Configuration
 @PropertySource("classpath:application.properties")
+@ImportResource("classpath:application.xml")
+@Import(WebConfiguration.class)
 @ComponentScan(
         basePackages = "com.iwor.spring",
         useDefaultFilters = false,
