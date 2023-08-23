39e9c55b337b913f38b8dac617efff7f78a4fd0a
Iw <iwor.java@gmail.com>
2023-08-23 11:45:06 +0400

    add locale to Login page

--- src/main/java/com/iwor/web/config/WebConfiguration.java
+++ src/main/java/com/iwor/web/config/WebConfiguration.java
@@ -1,8 +1,13 @@
 package com.iwor.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Profile("web")
 @Configuration
@@ -12,4 +17,23 @@ public class WebConfiguration implements WebMvcConfigurer {
 //    public void addFormatters(FormatterRegistry registry) {
 //        registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
 //    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        var localeResolver = new CookieLocaleResolver();
+//        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
 }
