ebec8574ce5a6d5b58cac9e58fe542b202519f77
Iw <iwor.java@gmail.com>
2023-08-23 11:44:34 +0400

    add locale rest endpoint

---
+++ src/main/java/com/iwor/spring/http/rest/MessageRestController.java
@@ -0,0 +1,24 @@
+package com.iwor.spring.http.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/messages")
+@RestController
+public class MessageRestController {
+
+    private final MessageSource messageSource;
+
+    @GetMapping
+    public String getMessage(@RequestParam String key,
+                             @RequestParam String lang) {
+        return messageSource.getMessage(key, null, null, new Locale(lang));
+    }
+}
